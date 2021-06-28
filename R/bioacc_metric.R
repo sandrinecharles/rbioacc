ebffa9206470467dfb6f8a81d3b8ec9d5212e46d
vbaudrot <virgile.baudrot@posteo.net>
2021-06-28 23:13:15 +0200

    add bioacc metric

--- R/bioacc_metric.R
+++ R/bioacc_metric.R
@@ -12,20 +12,53 @@ bioacc_metric <- function(fit, ...){
 #' 
 #' @export
 #' 
-bioacc_metric.fitTK <- function(fit){
+bioacc_metric.fitTK <- function(fit, object){
   
   fitMCMC <- rstan::extract(fit[["stanfit"]])
-  sum_ <- apply(fitMCMC$ke, 1, sum) + apply(fitMCMC$km, 1, sum)
-  BCF_ku <- lapply(1:ncol(fitMCMC$ku), function(i) fitMCMC$ku[,i] / sum_)
   
-  return(BCF_ku)
+  if(!is.null(fitMCMC$km)){
+    sum_ <- apply(fitMCMC$ke, 1, sum) + apply(fitMCMC$km, 1, sum)
+  } else{
+    sum_ <- apply(fitMCMC$ke, 1, sum)
+  }
+  
+  BCF_ku <- lapply(1:ncol(fitMCMC$ku), function(i) fitMCMC$ku[,i] / sum_)
+  names(BCF_ku) <- exposure_names(object)
+  
+  df <- data.frame(do.call("cbind", BCF_ku))
+  class(df) <- append("bioaccMetric", class(df))
+  
+  return(df)
 }
 
-#' Rertiev ku names from object
+#' Plot function for object of class \code{bioaccMetric}
 #'  
 #' @export
 #' 
-ku_names <- function(object){
+plot.bioaccMetric <- function(df){
+
+  df_plt <- .fonte(df, "exposure","BCF_ku")
+  
+  df_quant <- .fonte(.df_quant95(df), "Quantile", "BCF_ku")
+  df_quant$exposure <- rep(colnames(df), 3)
+  
+  plt <- ggplot() + 
+    theme_minimal() +
+    labs(x = "BCF", y = "Density") +
+    geom_density(data = df_plt,
+                 aes(x = BCF_ku, fill = exposure), fill = "grey", color = NA) +
+    geom_vline(data = df_quant,
+               aes(xintercept = BCF_ku, group = Quantile), linetype = "dashed") +
+    facet_wrap(~exposure, scale = "free")
+    
+  return(plt)
+}
+
+#' Retrieve exposure routes names from object
+#'  
+#' @export
+#' 
+exposure_names <- function(object){
   col_exposure <- .index_col_exposure(object)
   sub <- substring(names(object[,col_exposure]), first = 4)
   return(sub)
@@ -33,3 +66,13 @@ ku_names <- function(object){
 
 
 
+# ------------------- INTERNAL
+
+.fonte <- function(df, names_to, values_to){
+  dfout <- data.frame(
+    names_to = rep(colnames(df), each = nrow(df)),
+    values_to = do.call("c", lapply(1:ncol(df), function(i) df[,i]))
+  )
+  colnames(dfout) <- c(names_to, values_to)
+  return(dfout)
+}
--- tests/testthat/test-metric.R
+++ tests/testthat/test-metric.R
@@ -1,18 +1,34 @@
 test_that("bioacc_metric", {
   
+  # small test to run fast
   data("Male_Gammarus_Single")
   modelData_MGS <- modelData(Male_Gammarus_Single, time_accumulation = 4)
   fit_MGS <- fitTK(modelData_MGS, iter = 10, chains = 2)
   data("Male_Gammarus_seanine_growth")
   modelData_MGSG <- modelData(Male_Gammarus_seanine_growth, time_accumulation = 1.417)
   fit_MGSG <- fitTK(modelData_MGSG, iter = 10, chains=2)
-  
-  Chiro_Creuzot <- read.delim("C:/Users/virgi/OneDrive/Documents/PACKAGES/rbioacc/data-raw/Chiro_Creuzot.txt")
+  data("Chiro_Creuzot")
   Chiro_Creuzot <- Chiro_Creuzot[Chiro_Creuzot$replicate == 1,]
   modelData_CC <- modelData(Chiro_Creuzot, time_accumulation = 1.0)
-  
-  
   fit_CC <- fitTK(modelData_CC, iter = 10, chains=2)
   
+  BFC_MGS = bioacc_metric(fit_MGS, Male_Gammarus_Single)
+  BFC_MGSG = bioacc_metric(fit_MGSG, Male_Gammarus_seanine_growth)
+  BFC_CC = bioacc_metric(fit_CC, Chiro_Creuzot)
+  
+  # Check class
+  expect_true(all(class(BFC_MGS) == c("bioaccMetric", "data.frame")))
+  expect_true(all(class(BFC_MGSG) == c("bioaccMetric", "data.frame")))
+  expect_true(all(class(BFC_CC) == c("bioaccMetric", "data.frame")))
+  
+  # Check colnames
+  expect_true(all(colnames(BFC_MGS) == c("w")))
+  expect_true(all(colnames(BFC_MGSG) == c("w")))
+  expect_true(all(colnames(BFC_CC) == c("w","s","pw")))
+
+  # Check class
+  expect_true(all(class(plot(BFC_MGS)) == c("gg", "ggplot")))
+  expect_true(all(class(plot(BFC_MGSG)) == c("gg", "ggplot")))
+  expect_true(all(class(plot(BFC_CC)) == c("gg", "ggplot")))
   
 })
