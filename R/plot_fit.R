113b9201a4371123882435fb4f530baeff90cf62
vbaudrot <virgile.baudrot@posteo.net>
2021-06-29 22:05:38 +0200

    hack plot positive y

--- R/plot_fit.R
+++ R/plot_fit.R
@@ -11,13 +11,18 @@
 #' @import ggplot2
 #' 
 plot.fitTK <- function(fit){
-# plot_fitTK <- function(fit){
-  
+
   df <- .df_for_plot(fit)
   
+  # HACK TO BE > 0
+  df$q50 <- ifelse(df$q50<0,0,df$q50)
+  df$qinf95 <- ifelse(df$qinf95<0,0,df$qinf95)
+  df$qsup95 <- ifelse(df$qsup95<0,0,df$qsup95)
+
   plt <- ggplot(data = df) + 
     theme_classic() +
     labs(x = "Time", y = "Concentration") +
+    # scale_y_continuous(limits = c(0,NA)) +
     geom_ribbon(
       aes(x = time, ymin = qinf95, ymax = qsup95), fill = "grey80") +
     geom_line(aes(x = time, y = q50), color = "orange") +
@@ -25,6 +30,5 @@ plot.fitTK <- function(fit){
     facet_wrap(~variable, scales = "free")
 
   return(plt)
-  
 }
 
