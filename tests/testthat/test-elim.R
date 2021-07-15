7f678eaf8ecf2459a69e58f2bad63acc181eae80
vbaudrot <virgile.baudrot@posteo.net>
2021-07-15 10:35:19 +0200

    add test elim rate

--- tests/testthat/test-elim.R
+++ tests/testthat/test-elim.R
@@ -5,25 +5,31 @@ test_that("elimination_rate", {
   modelData_MGS_ke <- modelData(Male_Gammarus_Single, time_accumulation = 4)
   fit_MGS_ke <- fitTK(modelData_MGS_ke, iter = 1000, chains = 2)
   plot(fit_MGS_ke)
+  plot_PriorPost(fit_MGS_ke0)
   modelData_MGS_ke0 <- modelData(Male_Gammarus_Single, time_accumulation = 4, elimination_rate = 1e-9)
   fit_MGS_ke0 <- fitTK(modelData_MGS_ke0, iter = 1000, chains = 2)
   plot(fit_MGS_ke0)
   ppc(fit_MGS_ke0)
+  plot_PriorPost(fit_MGS_ke0)
+  
+  unique(rstan::extract(fit_MGS_ke0[["stanfit"]])$E) == 1e-9
   
   data("Male_Gammarus_seanine_growth")
   modelData_MGSG_ke <- modelData(Male_Gammarus_seanine_growth, time_accumulation = 1.417)
   fit_MGSG_ke <- fitTK(modelData_MGSG_ke, iter = 1000, chains=2)
+  plot(fit_MGSG_ke)
   modelData_MGSG_ke0 <- modelData(Male_Gammarus_seanine_growth, time_accumulation = 1.417, elimination_rate = 1e-9)
   fit_MGSG_ke0 <- fitTK(modelData_MGSG_ke0, iter = 1000, chains=2)
   plot(fit_MGSG_ke0)
   ppc(fit_MGSG_ke0)
   
+  unique(rstan::extract(fit_MGSG_ke0[["stanfit"]])$E) == 1e-9
+  
   data("Chiro_Creuzot")
   Chiro_Creuzot <- Chiro_Creuzot[Chiro_Creuzot$replicate == 1,]
   modelData_CC_ke0 <- modelData(Chiro_Creuzot, time_accumulation = 1.0, elimination_rate = 1e-9)
-  fit_CC_ke0 <- fitTK(modelData_CC_ke0, iter = 10, chains=2)
+  fit_CC_ke0 <- fitTK(modelData_CC_ke0, iter = 1000, chains=2)
   plot(fit_CC_ke0)
   ppc(fit_CC_ke0)
-  quantile_table(fit_CC_ke0)
   
 })
