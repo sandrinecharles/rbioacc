46f8af82cb68205c3b4aea90d25949725a56b781
vbaudrot <virgile.baudrot@posteo.net>
2021-09-10 14:43:17 +0200

    stop export .var_init

--- R/predict_fit.R
+++ R/predict_fit.R
@@ -9,6 +9,8 @@
 #' columns with name in \code{expw}, \code{exps}, \code{expf} and \code{exppw}
 #' @param \dots Additional arguments
 #' 
+#' @importFrom stats rnorm
+#' 
 #' @return An object of class \code{predictTK}
 #' 
 #' @export
@@ -162,12 +164,12 @@ predict.fitTK <- function(object, data, mcmc_size = NULL, fixed_init = TRUE, ...
   return(predict_out)
 }
 
-#' Additional variance for initial condition
-#' 
-#' @importFrom stats rnorm
-#' 
-#' @return a vector of numeric
-#' 
+# Additional variance for initial condition
+# 
+# @importFrom stats rnorm
+# 
+# @return a vector of numeric
+# 
 .var_init <- function(n,x,sd,fixed_init){
   if(fixed_init == TRUE){
     return(rnorm(n,x,sd))
